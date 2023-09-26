b13a14e689e1f7eebf0b9f294bf42e17d1521ca8
Victoria Treskova <vikatresk@yandex.ru>
2023-09-26 12:50:59 +0500

    fix result_flat.json rename files in fixtures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,7 +4,7 @@ from gendiff import generate_diff
 def test_json_flat_diff():
     file1 = './tests/fixtures/input/flat1.json'
     file2 = './tests/fixtures/input/flat2.json'
-    with open('./tests/fixtures/output/result_flat_json') as file:
+    with open('./tests/fixtures/output/result_flat_json.txt') as file:
         expected = file.read()
         diff = generate_diff(file1, file2)
         assert expected == diff
