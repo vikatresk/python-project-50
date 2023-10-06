2e80672bd7a8f18a69837e1836d1f25d536fa552
Victoria Treskova <vikatresk@yandex.ru>
2023-10-06 23:21:39 +0500

    fix lint

--- gendiff/get_format.py
+++ gendiff/get_format.py
@@ -3,7 +3,7 @@ from gendiff.formatter.plain import make_plain
 from gendiff.formatter.json import make_json
 
 
-def apply_format(data, formatter = 'stylish'):
+def apply_format(data, formatter='stylish'):
     if formatter == 'plain':
         return make_plain(data)
     if formatter == 'json':
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -4,7 +4,7 @@ import argparse
 def get_parser():
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows differences."
-        )
+    )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
