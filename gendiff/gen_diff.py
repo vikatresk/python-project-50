0ea2d607fda90730f95f692f0fa669a0492a2916
Victoria Treskova <vikatresk@yandex.ru>
2023-10-06 23:11:57 +0500

    make stylish default

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -3,8 +3,8 @@ from gendiff.get_format import apply_format
 from gendiff.build_diff import compare_dicts
 
 
-def generate_diff(first_file, second_file, format='stylish'):
+def generate_diff(first_file, second_file, formatter='stylish'):
     dict1 = load_file(first_file)
     dict2 = load_file(second_file)
     diff = compare_dicts(dict1, dict2)
-    return apply_format(diff, format)
+    return apply_format(diff, formatter)
--- gendiff/get_format.py
+++ gendiff/get_format.py
@@ -3,10 +3,9 @@ from gendiff.formatter.plain import make_plain
 from gendiff.formatter.json import make_json
 
 
-def apply_format(data, formatter):
-    if formatter == 'stylish':
-        return make_stylish(data)
+def apply_format(data, formatter = 'stylish'):
     if formatter == 'plain':
         return make_plain(data)
     if formatter == 'json':
         return make_json(data)
+    return make_stylish(data)
