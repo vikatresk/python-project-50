84477bc24d5095a054decc6aa4316c9867343c64
Victoria Treskova <vikatresk@yandex.ru>
2023-10-24 09:39:24 +0500

    refactor stylish, solve c901

--- gendiff/formatter/stylish.py
+++ gendiff/formatter/stylish.py
@@ -1,63 +1,53 @@
-import itertools
-
-
-def to_str(value):
-    if value in (False, True):
-        value = str(value).lower()
-    elif value is None:
-        value = 'null'
-    elif isinstance(value, dict):
-        for k, v in value.items():
-            v = to_str(v)
-    return value
-
-
 STATUS = {
     'unchanged': "  ",
     'added': "+ ",
     'deleted': "- "
 }
+REPLACER = " "
 
 
-def make_stylish(data, replacer=' ', spaces_count=4):  # noqa c901
-    def collect(current_value, depth):
-        if not isinstance(current_value, (list, dict)):
-            return str(current_value)
-        first_line_indent = replacer * (depth * spaces_count + 2)
-        last_line_indent = replacer * (depth * spaces_count + 4)
-        curr_line_indent = replacer * (depth * spaces_count)
+def to_str(value, spaces_count=2):
+    if value in (False, True):
+        return str(value).lower()
+    elif value is None:
+        return 'null'
+    elif isinstance(value, dict):
+        indent = REPLACER * (spaces_count + 4)
         lines = []
-        if isinstance(current_value, dict):
-            for k, v in current_value.items():
-                lines.append(f'{last_line_indent}{k}: '
-                             f'{collect(v, depth + 1)}')
-        for diff in current_value:
-            if 'status' in diff:
-                status = diff['status']
-                if status == 'changed':
-                    value1 = to_str(diff['value1'])
-                    value2 = to_str(diff['value2'])
-                    new_key1 = '- ' + diff['key']
-                    new_key2 = '+ ' + diff['key']
-                    lines.append(
-                        f'{first_line_indent}{new_key1}: '
-                        f'{collect(value1, depth + 1)}')
-                    lines.append(
-                        f'{first_line_indent}{new_key2}: '
-                        f'{collect(value2, depth + 1)}')
-                elif status == 'parent':
-                    child = to_str(diff['value'])
-                    new_key = '  ' + diff['key']
-                    lines.append(
-                        f'{first_line_indent}{new_key}: '
-                        f'{collect(child, depth + 1)}')
-                else:
-                    value = to_str(diff['value'])
-                    new_key = STATUS[diff['status']] + diff['key']
-                    lines.append(
-                        f'{first_line_indent}{new_key}: '
-                        f'{collect(value, depth + 1)}')
+        for k, v in value.items():
+            str_value = to_str(v, spaces_count + 4)
+            lines.append(f"{indent}{STATUS['unchanged']}{k}: {str_value}")
+        string = '\n'.join(lines)
+        end_indent = REPLACER * (spaces_count + 2)
+        return f"{{\n{string}\n{end_indent}}}"
+    return f"{value}"
 
-        result = itertools.chain("{", lines, [curr_line_indent + "}"])
-        return '\n'.join(result)
-    return collect(data, 0)
+
+def make_stylish(data, spaces_count=2):
+    lines = []
+    indent = REPLACER * spaces_count
+    for diff in data:
+        if 'status' in diff:
+            status = diff['status']
+            if status == 'changed':
+                value1 = to_str(diff['value1'], spaces_count)
+                value2 = to_str(diff['value2'], spaces_count)
+                new_key1 = '- ' + diff['key']
+                new_key2 = '+ ' + diff['key']
+                lines.append(
+                    f'{indent}{new_key1}: {value1}')
+                lines.append(
+                    f'{indent}{new_key2}: {value2}')
+            elif status == 'parent':
+                children = make_stylish(diff['value'], spaces_count + 4)
+                new_key = '  ' + diff['key']
+                lines.append(
+                    f'{indent}{new_key}: {children}')
+            else:
+                value = to_str(diff['value'], spaces_count)
+                new_key = STATUS[diff['status']] + diff['key']
+                lines.append(
+                    f'{indent}{new_key}: {value}')
+    stylish_string = '\n'.join(lines)
+    end_indent = REPLACER * (spaces_count - 2)
+    return f"{{\n{stylish_string}\n{end_indent}}}"
